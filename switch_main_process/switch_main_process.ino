42f790c14d5a593d207f2ca72efd0253d965b496
MBP10 <mbp10@Adminui-MacBook-Pro.local>
2018-06-03 00:05:22 +0900

    기능 추가 구현
    나머지 기능 추가 구현중. 어플리케이션과 연동 완료

--- switch_main_process/switch_main_process.ino
+++ switch_main_process/switch_main_process.ino
@@ -1,7 +1,9 @@
-
+#include <stdio.h>
 #include <SoftwareSerial.h>
 #include <Servo.h>
+//#include <stdint.h>
 #include <DS1302.h>
+#include <time.h>
 
 
 #define BT_RX 7 // 블루투스 송수신 포트
@@ -12,10 +14,10 @@
 #define Motor3Port 11
 
 #define ServoStandard 90 //서보모터 최초 초기화 각도설정
-#define ServoUp 0        //버튼 3개일때 서보 조정 각도
-#define ServoDown 180     //버튼 3개일때 서보 조정 각도
-#define ServoUp2 30       //버튼 2개일때 서보 조정 각도
-#define ServoDown2 120    //버튼 2개일때 서보 조정 각도
+#define ServoUp 102        //버튼 3개일때 서보 조정 각도
+#define ServoDown 76     //버튼 3개일때 서보 조정 각도
+#define ServoUp2 102       //버튼 2개일때 서보 조정 각도
+#define ServoDown2 76    //버튼 2개일때 서보 조정 각도
 #define ServoOn 30        //스위치를 움직이는 2개의 모터 조정 각도
 #define ServoOff 150
 #define Action 1000       //서보 움직임을 기다려주는 수치
@@ -24,17 +26,33 @@
 //int i,j=0;
 
 SoftwareSerial HM10(BT_RX,BT_TX);
+DS1302 rtc(6,5,4);
 Servo motor1;
 Servo motor2;
 Servo motor3;
+
+
+
+
+struct Time1 
+{
+  int yr;
+  int month;
+  int day;
+  int hr;
+  int minute;
+  int second;
+  int dayOfWeek;
+  
+};
 
 /*
 int switchStat1 = 0;
 int switchStat2 = 0;
 int switchStat3 = 0;*/
 
-int funcFlag1 = 0;
-int funcFlag2 = 0;
+int setFlag1 = 0;
+int setFlag2 = 0;
 
 int normalMode = 0;
 int elevatorMode = 0;
@@ -45,6 +63,22 @@ int initialStat3=0;
 
 int numberOfButton = 0;
 
+int stringnum[15] = {0};
+
+// 년월일시분초 함수에서 쓸거
+int setYr = 0;
+int setMonth = 0;
+int setDay = 0;
+int setHr = 0;
+int setMinute = 0;
+int setSc = 0;
+
+//시분초 함수에서 쓸거
+int setHr1 = 0;
+int setMinute1 = 0;
+int setSc1 = 0;
+
+
 
 
 String inputString;
@@ -53,8 +87,8 @@ String outputString;
 
 int modeInitialize(String inputStr);
 
-void alamSettingByTime(int hour, int minute ,int second);
-void alamSettingByDate(int year,int month, int date,int hour, int minute,int second);
+void alamSettingByTime(int hourIn, int minuteIn ,int secondIn);
+void alamSettingByDate(int yearIn,int monthIn, int dateIn,int hourIn, int minuteIn,int secondIn);
 
 void normalSwitchServoInitialSetting(int motorNum ,int initialStat);
 void normalSwitchServo(int switchNum,int stat);
@@ -62,9 +96,12 @@ void normalSwitchServo(int switchNum,int stat);
 void elevatorSwitchServoInitialSetting(int initialStat);
 void elevatorSwitchServo(int stat);
 
+
+
 void on();
 void off();
 
+//Time t = getTime();
 
 void setup() {
   // put your setup code here, to run once:
@@ -77,6 +114,7 @@ void setup() {
   while(1){
   
   Serial.println("initializing - wait for signal");
+  HM10.println("initializing - wait for signal");
   while(HM10.available()==0){
   
   }
@@ -91,6 +129,7 @@ void setup() {
   
   if (temp){
     Serial.print("complete!!");
+    HM10.print("complete!!");
     break;
     }
   }
@@ -113,8 +152,11 @@ void setup() {
 
   Serial.print("설정된 버튼수는 ");
   Serial.println(numberOfButton);
+  
   inputString.remove(0);
 }
+
+
 
 void loop(){
   //BT -> Data -> Serial
@@ -137,6 +179,11 @@ void loop(){
     //HM10.println(outputString.length());
     
   }
+
+  //Time1 t;
+  //t = rtc.
+  
+  
   //데이터 파싱
     
 
@@ -148,13 +195,49 @@ void loop(){
     //일반 스위치 모드
     char string0 = '0';
     char string1 = '0';
+    
     string0 = inputString[0];
     string1 = inputString[1];
-    int string0num = atoi(&string0);
-    int string1num = atoi(&string1);
+    stringnum[0] = atoi(&string0);
+    stringnum[1] = atoi(&string1);
 
-    normalSwitchServo(numberOfButton,string0num,string1num);
-    
+    if (string0 == 1 || string0 == 2 ||string0 ==3){
+
+    normalSwitchServo(numberOfButton,stringnum[0],stringnum[1]);
+    }
+
+    else if(string0 == 4){
+
+
+      for(int i =0; i<15;i++){
+      stringnum[i]=atoi(&inputString[i]);  
+      }
+      int yrIn = stringnum[1]*1000 + stringnum[2]*100+ stringnum[3]*10 + stringnum[4];
+      int monthIn = stringnum[5]*10 + stringnum[6];
+      int dayIn = stringnum[7]*10+stringnum[8];
+      int hrIn = stringnum[9]*10+stringnum[10];
+      int minuteIn = stringnum[11]*10+stringnum[12];
+      int secondIn = stringnum[13]*10+stringnum[14];
+      
+      alamSettingByTime(hrIn, minuteIn ,secondIn);
+      
+    }
+
+    else if(string0 == 5){
+      
+      for(int i =0; i<15;i++){
+      stringnum[i]=atoi(&inputString[i]);  
+      }
+
+      int yrIn = stringnum[1]*1000 + stringnum[2]*100+ stringnum[3]*10 + stringnum[4];
+      int monthIn = stringnum[5]*10 + stringnum[6];
+      int dayIn = stringnum[7]*10+stringnum[8];
+      int hrIn = stringnum[9]*10+stringnum[10];
+      int minuteIn = stringnum[11]*10+stringnum[12];
+      int secondIn = stringnum[13]*10+stringnum[14];
+      
+      alamSettingByDate(yrIn,monthIn,dayIn,hrIn,minuteIn,secondIn);
+    }
     
   
 
@@ -173,6 +256,11 @@ void loop(){
 
   inputString.remove(0);//string buffer 초기화
   outputString.remove(0);
+
+  for(int i=0;i<15;i++){
+  stringnum[i] = 0;
+  }
+  
 }
 
 
@@ -207,12 +295,12 @@ int modeInitialize(String inputStr){ // 처음 모드 설정해주기
   
 }
 
-void alamSettingByTime(int hour, int minute ,int second){
+void alamSettingByTime(int hourIn, int minuteIn ,int secondIn){
   
   
 }
 
-void alamSettingByDate(int year,int month, int date,int hour, int minute,int second){
+void alamSettingByDate(int yearIn,int monthIn, int dateIn,int hourIn, int minuteIn,int second){
   
   
 }
@@ -226,7 +314,7 @@ void normalSwitchServoInitialSetting(int motorNum ,int stat){
     motor3.attach(Motor3Port);
 
 
-    motor2.write(ServoOff);
+    motor2.write(ServoOn);
     delay(Action);
     motor3.write(ServoOff);
     delay(Action);
@@ -244,7 +332,6 @@ void normalSwitchServoInitialSetting(int motorNum ,int stat){
     initialStat1 = 0;
     initialStat2 = 0;
     initialStat3 = 0;
-    
     break;
     
     case 1:
@@ -268,7 +355,7 @@ void normalSwitchServoInitialSetting(int motorNum ,int stat){
     
     case 4:
     initialStat1 = 0;
-    initialStat2 = 1;
+    initialStat2 = 0;
     initialStat3 = 1;
     break;
     
@@ -291,6 +378,8 @@ void normalSwitchServoInitialSetting(int motorNum ,int stat){
     initialStat3 = 1;
     break;
 
+
+    
   }
 
 
@@ -315,6 +404,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       on();
       initialStat1 = 1;
       Serial.println("switch 1 on");
+      HM10.print("switch 1 on");
     }
     
     if(num == 1 &&stat == 0){
@@ -324,6 +414,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       off();
       initialStat1 = 0;
       Serial.println("switch 1 off");
+      HM10.print("switch 1 off");
     }
 
 
@@ -336,6 +427,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       on();
       initialStat2 = 1;
       Serial.println("switch 2 on");
+      HM10.print("switch 2 on");
     }
     if(num ==2&& stat ==0){
       motor1.write(ServoDown2);
@@ -343,6 +435,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       off();
       initialStat2 = 0;
       Serial.println("switch 2 off");
+      HM10.print("switch 2 off");
     }
   }
 
@@ -354,6 +447,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       on();
       initialStat1 = 1;
       Serial.println("switch 1 on");
+      HM10.print("switch 1 on");
     }
     if(num ==1 && stat ==0 && initialStat1 == 1){
       motor1.write(ServoUp);
@@ -361,6 +455,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       off();
       initialStat1 = 0;
       Serial.println("switch 1 off");
+      HM10.print("switch 1 off");
     }
     if(num ==2 && stat ==1 && initialStat2 == 0){
       motor1.write(ServoStandard);
@@ -368,6 +463,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       on();
       initialStat2 = 1;
       Serial.println("switch 2 on");
+      HM10.print("switch 2 on");
     }
     if(num ==2 && stat ==0 && initialStat2 == 1){
       motor1.write(ServoStandard);
@@ -375,6 +471,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       off();
       initialStat2 = 0;
       Serial.println("switch 2 off");
+      HM10.print("switch 2 off");
     }
     if(num ==3 && stat ==1 && initialStat3 == 0){
       motor1.write(ServoDown);
@@ -382,6 +479,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       on();
       initialStat3 = 1;
       Serial.println("switch 3 on");
+      HM10.print("switch 3 on");
     }
     if(num ==3 && stat ==0 && initialStat3 == 1){
       motor1.write(ServoDown);
@@ -389,6 +487,7 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
       off();
       initialStat3 = 0;
       Serial.println("switch 3 off");
+      HM10.print("switch 3 off");
     }
   }
 }
@@ -398,127 +497,6 @@ void normalSwitchServo(int switchNum, int num, int stat){ //switchNum 은 스위
 
 
 
-
-/*void normalSwitchServo(int motorNum,int stat){
-
-  if (motorNum == 1){
-    motor1.attach(Motor1Port);
-    
-    switch(stat){
-
-    case 0:
-    motor1.write(ServoOff);
-    break;
-    
-    case 1:
-    motor1.write(ServoOn);
-    break;
-
-    }
-  }
-  else if (motorNum == 2){
-    
-    motor1.attach(Motor1Port);
-    motor2.attach(Motor2Port);
-
-    switch(stat){
-
-    case 0:
-    motor1.write(ServoOff);
-    motor2.write(ServoOff);
-    break;
-    
-    case 1:
-    motor1.write(ServoOn);
-    motor2.write(ServoOff);
-    break;
-    
-    case 2:
-    motor1.write(ServoOff);
-    motor2.write(ServoOn);
-    break;
-    
-    case 3:
-
-    motor1.write(ServoOn);
-    motor2.write(ServoOn);
-    break;
-    
-    }
-  }
-
-  
-  else if (motorNum == 3){
-    
-    motor1.attach(Motor1Port);
-    motor2.attach(Motor2Port);
-    motor3.attach(Motor3Port);
-
-    switch(stat){
-    case 0:
-    motor1.write(ServoOff);
-    motor2.write(ServoOff);
-    motor3.write(ServoOff);
-    break;
-    
-    case 1:
-    motor1.write(ServoOn);
-    motor2.write(ServoOff);
-    motor3.write(ServoOff);
-    break;
-    
-    case 2:
-    motor1.write(ServoOff);
-    motor2.write(ServoOn);
-    motor3.write(ServoOff);
-    break;
-    
-    case 3:
-
-    motor1.write(ServoOn);
-    motor2.write(ServoOn);
-    motor3.write(ServoOff);
-    break;
-
-    case 4:
-
-    motor1.write(ServoOff);
-    motor2.write(ServoOff);
-    motor3.write(ServoOn);
-    break;
-
-    case 5:
-
-    motor1.write(ServoOn);
-    motor2.write(ServoOn);
-    motor3.write(ServoOff);
-    break;
-    
-    case 6:
-
-    motor1.write(ServoOff);
-    motor2.write(ServoOn);
-    motor3.write(ServoOn);
-    break;
-    
-    case 7:
-
-    motor1.write(ServoOn);
-    motor2.write(ServoOn);
-    motor3.write(ServoOn);
-    break;
-    
-    }
-  }
-
-  delay(1000);
-  
-  motor1.detach();
-  motor2.detach();
-  motor3.detach();
-
-  
-}*/
 
 
 
@@ -541,9 +519,9 @@ void on(){
 }
 
 void off(){
-  motor2.write(ServoOn);
-  delay(Action);
   motor2.write(ServoOff);
+  delay(Action);
+  motor2.write(ServoOn);
   delay(Action);
 }
 
